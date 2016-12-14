700e31d75d72ffc32226b6d1d53a654cf168584c
David Čepelík <david@cepelik.eu>
2016-12-14 12:54:03 +0100

    improved testing environment

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,35 +1,65 @@
 #!/usr/bin/env sh
 
-mcc=../../../src/mcc
-out_test=out-test
+prog=../../src/mcc
 
-cd cpp
+stdin=stdin
+stdout=stdout
+stderr=stderr
+
+stdout_test=$stdout-test
+stderr_test=$stderr-test
+
+num_ok=0
+num_errs=0
+
+test_file() {
+	if [ ! -f $1 ] || [ ! -f $2 ]; then
+		return
+	fi
+
+	diff $1 $2 >/dev/null
+
+	if [ $? -ne 0 ]; then
+		echo "    $1 differs"
+		num_errs=$(($num_errs + 1))
+	else
+		num_ok=$(($num_ok + 1))
+	fi
+}
+
+test_valgrind() {
+	valgrind_result=$(valgrind $prog $stdin 2>&1 >/dev/null | tail -n1 | cut -d ' ' -f4,10)
+
+	if [ "$valgrind_result" != "0 0" ]; then
+		num_errs=$(($num_errs + 1))
+		echo "    valgrind complains"
+	fi
+}
 
 for test in *; do
-	cd $test
-	printf "%-20s" $test
-
-	if [ ! -f out ]; then
-		echo "Missing output file to compare against"
-		rm -f $out_test
-		cd ..
+	if [ ! -d "$test" ]; then
 		continue
 	fi
 
-	$mcc 'in' > $out_test 2>/dev/null
+	cd $test
 
-	if [ $? != 0 ]; then
-		echo "Errors during compilation"
+	printf "%-20s\n" $test
+
+	$prog $stdin 2>$stderr_test | grep -v '^$' > $stdout_test
+	if [ $? -ne 0 ]; then
+		echo "    runtime error"
+		num_errs=$(($num_errs + 1))
 	else
-		diff out $out_test #>/dev/null
-
-		if [ $? != 0 ]; then
-			echo "Output differs"
-		else
-			echo "OK"
-		fi
+		test_file $stdout $stdout_test
+		test_file $stderr $stderr_test
 	fi
+	test_valgrind
 
-	rm -f $out_test
+	#rm -f -- $stdout_test
+	#rm -f -- $stderr_test
+
 	cd ..
 done
+
+echo
+echo "$num_ok OK, $num_errs FAILED"
